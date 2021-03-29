2a6469246551f725a12fa317ffda19285a6f8ce7
Jacky <jacky-943572677@qq.com>
2021-03-29 18:22:04 +0800

    新增清明放假安排

--- qi-frontend/src/components/About.vue
+++ qi-frontend/src/components/About.vue
@@ -10,10 +10,13 @@
         <p>项目地址：<a href="https://github.com/0xJacky/Qi">https://github.com/0xJacky/Qi</a></p>
         <p>公共测试阶段，bug 请联系 me@jackyu.cn</p>
         <h3>产品说明</h3>
-        <p>1. Qi 支持手动维护的放假、补课安排，配置信息在 holiday.py 目前支持2020年国庆及中秋节放假、补课安排，欢迎通过提交 PR 补充假期安排</p>
+        <p>1. Qi 支持手动维护的放假、补课安排，配置信息在 holiday.py，欢迎通过提交 PR 补充假期安排</p>
         <p>2. <strong>学号及密码仅用于请求教务系统</strong></p>
         <p>3. ics 文件导入 iOS 日历，请将 ics 通过邮件发送到 iOS 设备上，通过系统自带的邮件 App 可以直接将日历导入，建议在导入前新建一个新的日历分区</p>
         <p>4. macOS 用户可以直接导入 ics 文件</p>
+        <h3>补课安排</h3>
+        <p>1. 2020年国庆及中秋节放假、补课安排</p>
+        <p>2. 2021年元旦、清明放假安排</p>
     </div>
 </template>
 
